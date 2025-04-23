a5545ef086841bc0f398f8d651d105ac5a258c10
Valentin <valentinpuillandrepro@gmail.com>
2025-04-23 14:55:03 +0200

    fix linter errors

--- app/cinema_app/views.py
+++ app/cinema_app/views.py
@@ -3,13 +3,16 @@ from django.http import HttpResponse
 
 from .models import Movie, Viewer
 
+
 def index(request):
-   return HttpResponse("Hello, world. You're at the application index.")
+    return HttpResponse("Hello, world. You're at the application index.")
+
 
 def recent_movies(request):
-   movies = Movie.objects.order_by("-released")[:5]
-   return render(request, "recent_movies.html", {"movies": movies})
+    movies = Movie.objects.order_by("-released")[:5]
+    return render(request, "recent_movies.html", {"movies": movies})
+
 
 def viewers_list(request):
-   viewers = Viewer.objects.order_by("name")[:10]
-   return render(request, "viewers_list.html", {"viewers": viewers})
+    viewers = Viewer.objects.order_by("name")[:10]
+    return render(request, "viewers_list.html", {"viewers": viewers})
